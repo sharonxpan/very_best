62316bf3f301dd1967704f8b7c1dc95cbc429d60
Raghu Betina <raghu@firstdraft.com>
2018-12-03 01:56:40 +0000

    Indirect association between users and venues

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :venues,
+             :through => :bookmarks,
+             :source => :venues
+
   # Validations
 
   # Include default devise modules. Others available are:
